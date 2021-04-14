513c2ab2793325497ab1fabd50bb669e973d6e1a
dreadarceus <dreadarceus@gmail.com>
2021-04-14 10:14:06 +0530

    Upsolve: C, ABC 174

--- AtCoder Beginner's Contest/#174/C.cpp
+++ AtCoder Beginner's Contest/#174/C.cpp
@@ -1,48 +1,25 @@
 //MLE, i think the real solution is throught bit manipulation -2-August-2020
 #include <iostream>
-#include <vector>
+#include <string>
 #include <cmath>
 using namespace std;
-int numConvert(vector<char> v)
-{
-  int num = 0;
-  for(int i = 0; i < v.size(); i++)
-  {
-    num += 7 * pow(10, i);
-  }
-  return num;
-}
 int solve(int k)
 {
-  int i = k;
-  while(i % 2 == 0)
-  {
-    i /= 2;
-  }
-  if(i == 1)
-  {
-    return -1;
-  }
-  vector<char> num;
-  while(numConvert(num) < k)
-  {
-    num.push_back('7');
-  }
-  while(true)
-  {
-    int n = numConvert(num);
-    if(n % k == 0)
+    if (k % 2 == 0 || k % 5 == 0)
+        return -1;
+    long long ans = 7 % k;
+    int cnt = 1;
+    while (ans != 0)
     {
-      return num.size();
+        ans = (ans * 10 + 7) % k;
+        cnt++;
     }
-    num.push_back('7');
-  }
-  return -1;
+    return cnt;
 }
 int main()
 {
-  int k = 0;
-  cin >> k;
-  int ans = solve(k);
-  cout << ans;
+    int k = 0;
+    cin >> k;
+    int ans = solve(k);
+    cout << ans;
 }
